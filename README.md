72da505dbaf8ff0878c1fa82463b5b1e95f52d5a
Lightnet <darkneter@gmail.com>
2016-11-19 12:06:42 -0800

    simple button added.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # bgametools
+
+ Blender add on tool.
+ 
+ This is just my work build to have develop better testing for export fixed or remove ghosting data animation.
+
+ Download this and install from file for blender add on tool.
+ 
+ Features:
+  * work in progress.
--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-
-
-
+"""
+	Prototype builds...
+"""
 
 bl_info = {
     "name": "bgametools",
@@ -11,16 +11,52 @@ bl_info = {
     "description": "Simple Tool test.",
     "warning": "",
     'wiki_url': "",
-    "category": "testing"}
-	
+    "category": "testing"
+}
+
 import bpy
 import os
 import json
-from bpy_extras.io_utils import ExportHelper, ImportHelper
-from bpy.app.handlers import persistent
-import time
-import logging
+#from bpy_extras.io_utils import ExportHelper, ImportHelper
+#from bpy.app.handlers import persistent
+#import time
+#import logging
 
+class OBJECT_OT_animationclear(bpy.types.Operator):
+	bl_idname = "bgamemytools.animationclear"
+	bl_label = "Animation Clear Data"
+	bl_description = ''
+	#bl_context = 'objectmode'
+	def execute(self, context):
+		#print("test");
+		return {'FINISHED'}
+
+class OBJECT_OT_actionlist(bpy.types.Operator):
+	bl_idname = "bgamemytools.actionlist"
+	bl_label = "Action List"
+	bl_description = ''
+	#bl_context = 'objectmode'
+	def execute(self, context):
+		#print("test");
+		return {'FINISHED'}
+
+class OBJECT_OT_exportbabylonjs(bpy.types.Operator):
+	bl_idname = "bgamemytools.exportbabylonjs"
+	bl_label = "Export Babylonjs"
+	bl_description = ''
+	#bl_context = 'objectmode'
+	def execute(self, context):
+		#print("test");
+		return {'FINISHED'}
+
+class OBJECT_OT_exportobjjson(bpy.types.Operator):
+	bl_idname = "bgamemytools.exportobjjson"
+	bl_label = "Export Obj Json"
+	bl_description = ''
+	#bl_context = 'objectmode'
+	def execute(self, context):
+		#print("test");
+		return {'FINISHED'}
 
 class MytoolsPanel(bpy.types.Panel):
 	bl_label = "bgametools"
@@ -31,11 +67,18 @@ class MytoolsPanel(bpy.types.Panel):
 	bl_category = "testing"
 
 	def draw(self, context):
-		print("test")
+		#print("test")
 		scn = bpy.context.scene
 		box = self.layout.box()
-		box.label("Testing label")
+		box.label("Animation Data")
+		box.operator('bgamemytools.animationclear')
+		box.operator('bgamemytools.actionlist')
 
+		box = self.layout.box()
+		box.label("Export?")
+		box.operator('bgamemytools.exportbabylonjs')
+		box.operator('bgamemytools.exportobjjson')
+		#box.operator('bgamemytools.animationclear')
 
 def register():
     bpy.utils.register_module(__name__)
